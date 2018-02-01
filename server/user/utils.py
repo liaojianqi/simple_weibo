c406799cc3d956fa2b721aba7d9b8243e71a3492
liaojianqi <loinliao@163.com>
2018-02-01 15:40:16 +0800

    delete redundant

--- server/user/utils.py
+++ server/user/utils.py
@@ -5,9 +5,3 @@ import string
 # token generation
 def token_gen(n):
     return ''.join(random.choices(string.digits + string.ascii_letters, k=n))
-
-
-def auth(request):
-    token = request.META.get('token')
-    if token is None:
-        raise ValueError('')
